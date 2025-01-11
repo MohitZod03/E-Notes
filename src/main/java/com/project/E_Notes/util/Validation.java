75a53aec55b05d83a87102b5e37910812ed0371d
Mohit Zod <mohitzod03@gmail.com>
2025-01-11 15:06:55 +0530

    Notes Validation (me)

--- src/main/java/com/project/E_Notes/util/Validation.java
+++ src/main/java/com/project/E_Notes/util/Validation.java
@@ -2,6 +2,7 @@ package com.project.E_Notes.util;
 
 
 import com.project.E_Notes.dto.Categorydto;
+import com.project.E_Notes.dto.NotesDto;
 import com.project.E_Notes.exceptionHandling.ValidationException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -50,4 +51,46 @@ public class Validation {
             throw new ValidationException(error);
         }
     }
+
+
+    // Notes Validation  here.
+    public void notesValidation(NotesDto notesDto) {
+        // Map to collect all validation errors
+        Map<String, Object> error = new LinkedHashMap<>();
+
+        // Check if the DTO itself is null
+        if (ObjectUtils.isEmpty(notesDto)) {
+            throw new IllegalArgumentException("Notes object cannot be null or empty");
+        }
+
+        // Validate the 'name' field
+        if (ObjectUtils.isEmpty(notesDto.getTitle())) {
+            error.put("Title", "Title field is empty or null");
+        } else {
+            if (notesDto.getTitle().length() < 5) {
+                error.put("Title", "Title length must be at least 5 characters");
+            }
+            if (notesDto.getTitle().length() > 100) {
+                error.put("Title", "Title length must not exceed 100 characters");
+            }
+        }
+
+        // Validate the 'description' field
+        if (ObjectUtils.isEmpty(notesDto.getDescription())) {
+            error.put("description", "Description field is empty or null");
+        }
+
+        // Validate `categoryId`
+        if (ObjectUtils.isEmpty(notesDto.getCategory())) {
+            error.put("categoryId", "Category ID is missing");
+        }
+
+
+        // If there are Not Empty errors,then throw ValidationException
+        if (!error.isEmpty()) {
+            throw new ValidationException(error);
+        }
+    }
+
+
 }
